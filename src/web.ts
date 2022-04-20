48b620271b1012137bcc23a6090926ebd44c1628
Dmitriy Savin <dsavin@twilio.com>
2022-04-20 22:33:38 +0300

    Fix web process

--- src/web.ts
+++ src/web.ts
@@ -2,14 +2,19 @@
 // https://help.heroku.com/P1AVPANS/why-is-my-node-js-app-crashing-with-an-r10-error
 
 import * as http from 'http'
+import { IncomingMessage } from 'http'
 
 const host = 'localhost'
 const port: number = (process.env.PORT as unknown as number) || 5000
 
-const requestListener = function (req: any, res: any) {}
+const requestListener = function (req: IncomingMessage, res: any) {
+  console.log(req.url)
+  res.writeHead(200)
+  res.end()
+}
 
 const server = http.createServer(requestListener)
 
-server.listen(host, port, () => {
+server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`)
 })
