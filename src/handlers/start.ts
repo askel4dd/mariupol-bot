41571d38b55f15c37460f35949b09054cabb268d
Dmitriy Savin <dsavin@twilio.com>
2022-04-24 13:40:11 +0300

    Check if channel is private

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -2,6 +2,12 @@ import { Context } from '@/models/Context'
 import sendOptions from '@/helpers/sendOptions'
 
 export function handleStartQuestionnaire(ctx: Context) {
+    if (ctx.update.message?.chat.type !== 'private') {
+        ctx.reply(
+            'Извините, в данный момент бот поддерживает только приватные сообщения'
+        )
+        return
+    }
     ctx.session.questionnaire.start()
     return ctx.replyWithLocalization('ask_for_contact', sendOptions(ctx))
 }
